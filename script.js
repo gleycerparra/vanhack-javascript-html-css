b526d9757c56e2854bac3885df3cb49b6034eaa9
Gleycer Parra <gleycerparra@gmail.com>
2020-09-07 19:38:28 -0500

    new changes

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const events = [
   {
+    id: 1,
     title: "The Present and Future of Technology in Business Events",
     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ut libero odio. Nam pulvinar euismod erat eu rutrum. Ut porta risus non erat convallis iaculis. Ut ac ultricies est. Sed maximus sem augue, ut convallis dui pretium eu. Ut in quam molestie, tincidunt sapien in, gravida quam. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vestibulum feugiat sit amet sapien ac iaculis. Ut sollicitudin malesuada aliquet. Fusce pharetra feugiat nisl. Donec laoreet, sem mollis interdum congue, urna est ornare sapien, eu cursus ante massa sed lorem. Vestibulum sit amet mauris tempor, aliquam tellus et, cursus mauris. Maecenas eu ultrices augue. Duis pellentesque ultricies elit ut luctus.",
     location: "Ohio - EEUU",
@@ -8,9 +9,11 @@ const events = [
     color: "#FFFFFF",
     specialStyle: 'border: 4px solid #2CF06F',
     sharedIcon: 'https://svgshare.com/i/PQj.svg',
-    imageUrl: 'https://www.pcma.org/wp-content/uploads/2017/07/Event-Technology.jpg'
+    imageUrl: 'https://www.pcma.org/wp-content/uploads/2017/07/Event-Technology.jpg',
+    date: 'January 10, 2020'
   },
   {
+    id: 2,
     title: "Virtual Hiring Event for Women",
     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ut libero odio. Nam pulvinar euismod erat eu rutrum. Ut porta risus non erat convallis iaculis. Ut ac ultricies est. Sed maximus sem augue, ut convallis dui pretium eu. Ut in quam molestie, tincidunt sapien in, gravida quam. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vestibulum feugiat sit amet sapien ac iaculis. Ut sollicitudin malesuada aliquet. Fusce pharetra feugiat nisl. Donec laoreet, sem mollis interdum congue, urna est ornare sapien, eu cursus ante massa sed lorem. Vestibulum sit amet mauris tempor, aliquam tellus et, cursus mauris. Maecenas eu ultrices augue. Duis pellentesque ultricies elit ut luctus.",
     location: "Vancouver - Canada",
@@ -19,9 +22,11 @@ const events = [
     color: "#FFFFFF",
     specialStyle: 'border: 4px solid #F07E2C',
     sharedIcon: 'https://svgshare.com/i/PQj.svg',
-    imageUrl: 'https://vanhackblobstorageprod.blob.core.windows.net/img/events/thumbnail/cb0ec66f-0f9f-4821-81b9-27056473c7b3.jpg'
+    imageUrl: 'https://vanhackblobstorageprod.blob.core.windows.net/img/events/thumbnail/cb0ec66f-0f9f-4821-81b9-27056473c7b3.jpg',
+    date: 'August 25, 2020'
   },
   {
+    id: 3,
     title: "VanHackton 2020",
     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ut libero odio. Nam pulvinar euismod erat eu rutrum. Ut porta risus non erat convallis iaculis. Ut ac ultricies est. Sed maximus sem augue, ut convallis dui pretium eu. Ut in quam molestie, tincidunt sapien in, gravida quam. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vestibulum feugiat sit amet sapien ac iaculis. Ut sollicitudin malesuada aliquet. Fusce pharetra feugiat nisl. Donec laoreet, sem mollis interdum congue, urna est ornare sapien, eu cursus ante massa sed lorem. Vestibulum sit amet mauris tempor, aliquam tellus et, cursus mauris. Maecenas eu ultrices augue. Duis pellentesque ultricies elit ut luctus.",
     location: "Online",
@@ -30,9 +35,11 @@ const events = [
     color: "#FFFFFF;",
     specialStyle: 'border: 4px solid #2C98F0',
     sharedIcon: 'https://svgshare.com/i/PQj.svg',
-    imageUrl: 'https://blogs.iadb.org/conocimiento-abierto/wp-content/uploads/sites/10/2015/08/Hackaton-Aprendizaje.jpg'
+    imageUrl: 'https://blogs.iadb.org/conocimiento-abierto/wp-content/uploads/sites/10/2015/08/Hackaton-Aprendizaje.jpg',
+    date: 'All October, 2020'
   },
   {
+    id: 4,
     title: "Premium Open House Experience - September",
     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ut libero odio. Nam pulvinar euismod erat eu rutrum. Ut porta risus non erat convallis iaculis. Ut ac ultricies est. Sed maximus sem augue, ut convallis dui pretium eu. Ut in quam molestie, tincidunt sapien in, gravida quam. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vestibulum feugiat sit amet sapien ac iaculis. Ut sollicitudin malesuada aliquet. Fusce pharetra feugiat nisl. Donec laoreet, sem mollis interdum congue, urna est ornare sapien, eu cursus ante massa sed lorem. Vestibulum sit amet mauris tempor, aliquam tellus et, cursus mauris. Maecenas eu ultrices augue. Duis pellentesque ultricies elit ut luctus.",
     location: "Online",
@@ -40,64 +47,38 @@ const events = [
     backgroundColor: "#9F9B3E;",
     color: "#FFFFFF;",
     sharedIcon: 'https://svgshare.com/i/PQj.svg',
-    imageUrl: 'https://vanhackblobstorageprod.blob.core.windows.net/img/events/thumbnail/5f6f32fb-81cf-421c-87dc-d55edf33db4c.jpg'
+    imageUrl: 'https://vanhackblobstorageprod.blob.core.windows.net/img/events/thumbnail/5f6f32fb-81cf-421c-87dc-d55edf33db4c.jpg',
+    date: 'September 5, 2020'
   },
   {
+    id: 5,
     title: "Virtual Hiring Event for Women",
     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ut libero odio. Nam pulvinar euismod erat eu rutrum. Ut porta risus non erat convallis iaculis. Ut ac ultricies est. Sed maximus sem augue, ut convallis dui pretium eu. Ut in quam molestie, tincidunt sapien in, gravida quam. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vestibulum feugiat sit amet sapien ac iaculis. Ut sollicitudin malesuada aliquet. Fusce pharetra feugiat nisl. Donec laoreet, sem mollis interdum congue, urna est ornare sapien, eu cursus ante massa sed lorem. Vestibulum sit amet mauris tempor, aliquam tellus et, cursus mauris. Maecenas eu ultrices augue. Duis pellentesque ultricies elit ut luctus.",
-    location: "Madrid - Spain",
+    location: "Online",
     type: "MeetUp",
     backgroundColor: "#D9FFE3;",
     color: "#455A64",
     sharedIcon: 'https://svgshare.com/i/PLq.svg',
-    imageUrl: 'https://miro.medium.com/max/1000/0*Qfd9LKC5xvOTAXzd.png'
+    imageUrl: 'https://www.meetup.com/blog/wp-content/uploads/2020/03/online-meeting-digital-image.jpg',
+    date: 'September 10, 2020'
   },
   {
+    id: 6,
     title: "Virtual Hiring Event for Women",
     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ut libero odio. Nam pulvinar euismod erat eu rutrum. Ut porta risus non erat convallis iaculis. Ut ac ultricies est. Sed maximus sem augue, ut convallis dui pretium eu. Ut in quam molestie, tincidunt sapien in, gravida quam. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vestibulum feugiat sit amet sapien ac iaculis. Ut sollicitudin malesuada aliquet. Fusce pharetra feugiat nisl. Donec laoreet, sem mollis interdum congue, urna est ornare sapien, eu cursus ante massa sed lorem. Vestibulum sit amet mauris tempor, aliquam tellus et, cursus mauris. Maecenas eu ultrices augue. Duis pellentesque ultricies elit ut luctus.",
-    location: "Madrid - Spain",
+    location: "Online",
     type: "Open Webinar",
     backgroundColor: "#FCEDC2;",
     color: "#455A64",
-    sharedIcon: 'https://svgshare.com/i/PLq.svg'
+    sharedIcon: 'https://svgshare.com/i/PLq.svg',
+    imageUrl: 'https://elearningindustry.com/wp-content/uploads/2020/06/webinar-based-training-and-helpful-strategies.jpg',
+    date: 'October 12, 2020'
   },
 ];
+let appliedEvents = [];
 
 const eventCardTemplate = document.createElement("template");
-eventCardTemplate.innerHTML = `
-                <style>
-                :root {
-                    --indigo-dye: hsla(216, 39%, 28%, 1);
-                    --melon: hsla(10, 76%, 80%, 1);
-                    --cambridge-blue: hsla(122, 18%, 68%, 1);
-                    --international-orange-golden-gate-bridge: hsla(6, 71%, 44%, 1);
-                    --spanish-bistre: hsla(49, 51%, 34%, 1);
-                    --leap-event: lavender;
-                }
-                         
-                * {
-                    margin: 0;
-                }
-                    
-                .event-card {
-                    display: flex;
-                    margin: 16px;
-                    background: white;
-                    border-radius: 15px;
-                    box-shadow: 0px 0px 2px 1px rgba(0, 0, 0, 0.1);
-                    flex-direction: column;
-                    overflow: hidden;
-                    width: 304px;
-                    color: #455A64;
-                }
-                
-                
-                </style>
-                <div class="event-card">
-                    <slot name="card-content"></slot>
-                </div>
-`;
-
+eventCardTemplate.innerHTML = `<slot name="card-content"></slot>`;
 class EventCard extends HTMLElement {
   constructor() {
     super();
@@ -107,9 +88,27 @@ class EventCard extends HTMLElement {
     this.attachShadow({ mode: "open" });
     this.shadowRoot.appendChild(clone);
   }
-}
 
+}
 customElements.define("event-card", EventCard);
+
+
+const eventCardDialogTemplate = document.createElement("template");
+eventCardDialogTemplate.innerHTML = `<slot name="dialog-content"></slot>`;
+class EventCardDialog extends HTMLElement {
+  constructor() {
+    super();
+  }
+
+  connectedCallback() {
+    const clone = document.importNode(eventCardDialogTemplate.content, true);
+    this.attachShadow({ mode: "open" });
+    this.shadowRoot.appendChild(clone);
+  }
+}
+customElements.define("event-card-dialog", EventCardDialog);
+
+
 
 const container = document.getElementById("events-container");
 events.forEach((event, index) => {
@@ -132,12 +131,13 @@ events.forEach((event, index) => {
 
   eventCard.innerHTML = `
     <div slot="card-content">
+    <div class="event-card" onclick="showModal(${event.id})" style="${event.specialStyle}">
       <div class="event-card-header">
           ${premiumOnly}
           <img class="event-card-header-img"
             src="${event.imageUrl}" />
       </div>
-      <div class="event-info" style="${event.specialStyle}">
+      <div class="event-info">
           <div style="margin-bottom: 8px">
               <a href="#">
                 ${event.title}
@@ -145,11 +145,11 @@ events.forEach((event, index) => {
           </div>
           <div class="event-info-container">
             <img src="https://svgshare.com/i/PM8.svg" class="event-info-icon" alt="calendar icon" />
-            <slot name="date" class="event-date">Thursday, January 10th, 4:00 am</slot>
+            <div class="event-date">${event.date}</div>
           </div>
           <div class="event-info-container">
-              <img src="https://svgshare.com/i/PPY.svg" class="event-info-icon" alt="location icon" />
-              <div name="location" class="event-date">${event.location}</div>
+              <img src="https://svgshare.com/i/PPt.svg" class="event-info-icon" alt="location icon" />
+              <div class="event-date">${event.location}</div>
           </div>
           <div class="description">
               <p>${event.description}
@@ -157,5 +157,70 @@ events.forEach((event, index) => {
           </div>
           ${footer}
       </div>
+      </div>
     </div>`;
 });
+
+function showModal(id) {
+  const event = events.find(item => item.id === id);
+  const body = document.getElementsByTagName('body')[0];
+  const dialog = body.appendChild(new EventCardDialog());
+  dialog.innerHTML = `
+  <dialog slot="dialog-content" id="dialog">
+  <img src="https://svgshare.com/i/PR5.svg" onclick="closeModal()" class="dialog-close-btn" alt="close button" />
+
+  <div class="dialog-container">
+      <div class="dialog-img-container" style="background: url('${event.imageUrl}') no-repeat; background-size: cover">
+          <div class="dialog-img-footer" style="background: #2CF06F">
+              <span style="margin-left: 16px">
+                  <div name="type" style="color: ${event.color}; font-weight: bold; font-size: 16px;">
+                      ${event.type}</div>
+              </span>
+              <img src="${event.sharedIcon}" alt="shared"
+                  style="margin-right: 12px; cursor: pointer" />
+          </div>
+      </div>
+      <div class="dialog-info-container">
+          <div class="dialog-event-type">The event</div>
+          <div class="dialog-event-title">${event.title}
+          </div>
+          <div class="event-info-container">
+              <img src="https://svgshare.com/i/PM8.svg" class="event-info-icon" alt="calendar icon" />
+              <div class="event-date">January 10, 2020</div>
+          </div>
+          <div class="event-info-container">
+              <img src="https://svgshare.com/i/PPt.svg" class="event-info-icon" alt="location icon" />
+              <div class="event-date">Online</div>
+          </div>
+          <p class="dialog-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Aenean ut
+              libero odio. Nam pulvinar euismod erat eu rutrum. Ut porta risus non erat convallis
+              iaculis.
+              Ut ac ultricies est. Sed maximus sem augue, ut convallis dui pretium eu. Ut in quam
+              molestie, tincidunt sapien in, gravida quam. Orci varius natoque penatibus et magnis dis
+              parturient montes, nascetur ridiculus mus. Vestibulum feugiat sit amet sapien ac
+              iaculis. Ut
+              sollicitudin malesuada aliquet.
+          </p>
+          <button id="apply-button" onclick="applyButton(events[0])" class="dialog-btn">
+              Apply
+          </button>
+      </div>
+  </div>
+</dialog>
+  `
+
+  /* const event = events.find(item => item.id === id);
+  console.log(event); */
+  window.dialog.showModal();
+
+}
+
+function closeModal() {
+  var currentDialog = document.getElementById("dialog");
+  currentDialog.remove();
+}
+
+function applyButton(event) {
+  console.log(event);
+}
